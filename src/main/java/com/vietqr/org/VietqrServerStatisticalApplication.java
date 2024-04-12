0b755e5b859b31d5df6f6581bd255ab308a3babf
admin <duchieupham1709@gmail.com>
2024-04-12 09:21:16 +0700

    initial project

---
+++ src/main/java/com/vietqr/org/VietqrServerStatisticalApplication.java
@@ -0,0 +1,13 @@
+package com.vietqr.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietqrServerStatisticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VietqrServerStatisticalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vietqr/org/VietqrServerStatisticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vietqr.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VietqrServerStatisticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
